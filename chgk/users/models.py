7ff95ec6fd1050dbbbcd90ecfd9b5b0dacf973d5
Alexion <alex24bryant@mail.ru>
2022-08-17 12:21:36 +0500

    update user models

--- chgk/users/models.py
+++ chgk/users/models.py
@@ -20,7 +20,7 @@ class CustomUserManager(BaseUserManager):
         return user
 
     def create_user(self, username, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_active', True)
         extra_fields.setdefault('is_superuser', False)
         return self._create_user(username, password, **extra_fields)
